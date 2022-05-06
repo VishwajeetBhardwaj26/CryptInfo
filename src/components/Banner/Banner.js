0d51e2adf4669c647e48eede5e4faeee87295202
vishwajeet bhardwaj <hwajeetbhabhardwaj@gmail.com>
2022-05-06 19:37:13 +0530

    deploy

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypt Info
           </Typography>
           <Typography
             variant="subtitle2"
